155c71a52d0641aeeb8f9c5a9371dfafd23ec4a7
Navezm <navez.martin@gmail.com>
2021-01-19 17:19:07 +0100

    exo 1er commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App() {
+import React, {useState} from 'react';
+
+let App = () => {
+  const [chiffre, setChiffre] = useState({string: 1});
   return (
     <div className="App">
-      
+      <span>{chiffre.string}</span> <br/>
+      <button>Click-icé</button>
     </div>
   );
-}
+};
 
 export default App;
